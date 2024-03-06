fe94f21580de1578d73689a62f7ef4f7b674b2df
Pásztor Dániel <danim1130@gmail.com>
2024-03-06 16:24:56 +0100

    Persist client information, handle message

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-let currentTimeoutId = undefined;
 let isEnabled = false;
 let controller = undefined;
 let messagePorts = new Set();
@@ -7,21 +5,31 @@ let messagePorts = new Set();
 const { Client } = require("@xhayper/discord-rpc")
 
 let client;
+let clientId;
+let clientSecret;
 
-function initializeDiscord(clientId, clientSecret) {
-
-  // const clientId = "1209211597017452594"; // <-- kkerti demo client id
-  // const clientSecret = "dD9P92Hcj_9TgrPdChi700fXRIrvho15" // <-- kkerti demo secret
+function initializeDiscord(newClientId, newClientSecret) {
   const redirect_uri = ""
 
   const scopes = ["rpc", "rpc.voice.read", "rpc.voice.write"];
+
+	clientId = newClientId;
+	clientSecret = newClientSecret
+
+  controller?.sendMessageToRuntime({
+	  id: "persist-data",
+	  data: {
+		  "client-id" : clientId,
+		  "client-secret" : clientSecret
+	  }
+  })
 
   client = new Client({
     clientId,
     clientSecret,
     redirect_uri
   });
-
+  
   client.login({ scopes })
     .then(async () => {
       const settings = await client.user.getVoiceSettings()
@@ -34,9 +42,7 @@ function initializeDiscord(clientId, clientSecret) {
           type: "echo",
           message: "Discord connected",
         })
-      }
-
-      );
+      });
     })
     .catch((error) => {
       messagePorts.forEach((port) => {
@@ -55,15 +61,15 @@ exports.loadPackage = async function (
   persistedData
 ) {
   controller = gridController;
+  clientId = persistedData?.["client-id"];
+  clientSecret = persistedData?.["client-secret"];
   isEnabled = true;
-  runLoop();
 };
 
 exports.unloadPackage = async function () {
   controller = undefined;
   messagePorts.forEach((port) => port.close());
   messagePorts.clear();
-  cancelLoop();
 };
 
 exports.addMessagePort = async function (port) {
@@ -72,11 +78,37 @@ exports.addMessagePort = async function (port) {
   });
 
   messagePorts.add(port);
+  if (clientId && clientSecret){
+	  port.postMessage({
+		  type: "clientInit",
+		  message: {
+			  clientId, clientSecret,
+		  }
+	  });
+  }
   port.on("close", () => {
     messagePorts.delete(port);
   });
   port.start();
 };
+
+exports.sendMessage = async function (args) {
+	let type = args[0];
+	if (type === "input"){
+		let vol = Number(args[1])
+		// vol must be between 0 and 100
+		if (vol > 100) vol = 100;
+		if (vol < 0) vol = 0;
+		client.user.setVoiceSettings({ input: { volume: vol } }).catch(console.info);
+	}
+	if (type === "output"){
+		let vol = Number(args[1])
+		// vol must be between 0 and 100
+		if (vol > 100) vol = 100;
+		if (vol < 0) vol = 0;
+		client.user.setVoiceSettings({ output: { volume: vol } }).catch(console.info);
+	}
+}
 
 async function onMessage(port, data) {
   if (data.type === "request-echo") {
@@ -102,22 +134,4 @@ async function onMessage(port, data) {
     // vol must be between 0 and 100
     client.user.setVoiceSettings({ output: { volume: vol } }).catch(console.info);
   }
-}
-
-async function runLoop() {
-  if (!isEnabled) return;
-
-  messagePorts.forEach((port) =>
-  // port.postMessage({
-  //   type: "echo",
-  //   message: "Loop message",
-  // })
-  { }
-  );
-  currentTimeoutId = setTimeout(runLoop, 2000);
-}
-
-function cancelLoop() {
-  isEnabled = false;
-  clearTimeout(currentTimeoutId);
 }
