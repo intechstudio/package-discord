21b2fb97a41d23c0354a20bd189d8d51779e45c9
Pásztor Dániel <danim1130@gmail.com>
2024-03-27 17:41:54 +0100

    Add build files

--- index.js
+++ index.js
@@ -2,46 +2,51 @@ let isEnabled = false;
 let controller = undefined;
 let messagePorts = new Set();
 
-const { Client } = require("@xhayper/discord-rpc")
+const { Client } = require("@xhayper/discord-rpc");
 
 let client;
 let clientId;
 let clientSecret;
 
-function initializeDiscord(newClientId, newClientSecret, refreshToken = undefined) {
-  const redirect_uri = ""
+function initializeDiscord(
+  newClientId,
+  newClientSecret,
+  refreshToken = undefined,
+) {
+  const redirect_uri = "";
 
   const scopes = ["rpc", "rpc.voice.read", "rpc.voice.write"];
 
-	clientId = newClientId;
-	clientSecret = newClientSecret
+  clientId = newClientId;
+  clientSecret = newClientSecret;
 
   client = new Client({
     clientId,
     clientSecret,
-    redirect_uri
+    redirect_uri,
   });
-  
-  client.login({ scopes, refreshToken })
+
+  client
+    .login({ scopes, refreshToken })
     .then(async () => {
       controller?.sendMessageToRuntime({
         id: "persist-data",
         data: {
-          "client-id" : clientId,
-          "client-secret" : clientSecret,
-          "refresh-token" : client.user.client.refreshToken,
-        }
-      })
-      const settings = await client.user.getVoiceSettings()
+          "client-id": clientId,
+          "client-secret": clientSecret,
+          "refresh-token": client.user.client.refreshToken,
+        },
+      });
+      const settings = await client.user.getVoiceSettings();
       messagePorts.forEach((port) => {
         port.postMessage({
           type: "init",
           message: { mic: settings.input.volume, out: settings.output.volume },
-        })
+        });
         port.postMessage({
           type: "echo",
           message: "Discord connected",
-        })
+        });
       });
     })
     .catch((error) => {
@@ -49,23 +54,18 @@ function initializeDiscord(newClientId, newClientSecret, refreshToken = undefine
         port.postMessage({
           type: "echo",
           message: "Discord connection failed" + error,
-        })
-      }
-      );
+        });
+      });
     });
-
 }
 
-exports.loadPackage = async function (
-  gridController,
-  persistedData
-) {
+exports.loadPackage = async function (gridController, persistedData) {
   controller = gridController;
   clientId = persistedData?.["client-id"];
   clientSecret = persistedData?.["client-secret"];
   let token = persistedData?.["refresh-token"];
-  if (token){
-    initializeDiscord(clientId, clientSecret, token)
+  if (token) {
+    initializeDiscord(clientId, clientSecret, token);
   }
   isEnabled = true;
 };
@@ -82,13 +82,14 @@ exports.addMessagePort = async function (port) {
   });
 
   messagePorts.add(port);
-  if (clientId && clientSecret){
-	  port.postMessage({
-		  type: "clientInit",
-		  message: {
-			  clientId, clientSecret,
-		  }
-	  });
+  if (clientId && clientSecret) {
+    port.postMessage({
+      type: "clientInit",
+      message: {
+        clientId,
+        clientSecret,
+      },
+    });
   }
   port.on("close", () => {
     messagePorts.delete(port);
@@ -97,22 +98,26 @@ exports.addMessagePort = async function (port) {
 };
 
 exports.sendMessage = async function (args) {
-	let type = args[0];
-	if (type === "input"){
-		let vol = Number(args[1])
-		// vol must be between 0 and 100
-		if (vol > 100) vol = 100;
-		if (vol < 0) vol = 0;
-		client.user.setVoiceSettings({ input: { volume: vol } }).catch(console.info);
-	}
-	if (type === "output"){
-		let vol = Number(args[1])
-		// vol must be between 0 and 100
-		if (vol > 100) vol = 100;
-		if (vol < 0) vol = 0;
-		client.user.setVoiceSettings({ output: { volume: vol } }).catch(console.info);
-	}
-}
+  let type = args[0];
+  if (type === "input") {
+    let vol = Number(args[1]);
+    // vol must be between 0 and 100
+    if (vol > 100) vol = 100;
+    if (vol < 0) vol = 0;
+    client.user
+      .setVoiceSettings({ input: { volume: vol } })
+      .catch(console.info);
+  }
+  if (type === "output") {
+    let vol = Number(args[1]);
+    // vol must be between 0 and 100
+    if (vol > 100) vol = 100;
+    if (vol < 0) vol = 0;
+    client.user
+      .setVoiceSettings({ output: { volume: vol } })
+      .catch(console.info);
+  }
+};
 
 async function onMessage(port, data) {
   if (data.type === "request-echo") {
@@ -125,17 +130,21 @@ async function onMessage(port, data) {
     initializeDiscord(data.clientId, data.clientSecret);
   }
   if (data.type === "mic_volume") {
-    let vol = Number(data.volume)
+    let vol = Number(data.volume);
     // vol must be between 0 and 100
     if (vol > 100) vol = 100;
     if (vol < 0) vol = 0;
-    client.user.setVoiceSettings({ input: { volume: vol } }).catch(console.info);
+    client.user
+      .setVoiceSettings({ input: { volume: vol } })
+      .catch(console.info);
   }
   if (data.type === "out_volume") {
-    let vol = Number(data.volume)
+    let vol = Number(data.volume);
     if (vol > 100) vol = 100;
     if (vol < 0) vol = 0;
     // vol must be between 0 and 100
-    client.user.setVoiceSettings({ output: { volume: vol } }).catch(console.info);
+    client.user
+      .setVoiceSettings({ output: { volume: vol } })
+      .catch(console.info);
   }
 }
